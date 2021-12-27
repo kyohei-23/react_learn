6d57c30fcdec704986cc4e633a777b6edcef319b
kyoheiS <tgifz@icloud.com>
2021-12-27 12:15:07 +0900

    初期の内容の破棄

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Button from './components/button';
+import Clock from './components/Clock';
+
+function Hello({ name }){
+  return <h2> Hello,{ name } </h2>
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const hoge = <h1>Hello,world!{ count }</h1>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      { hoge }
+      <Button />
+      <Clock />
+      <Hello name="hoge" />
+      <Hello name="huga" />
+      <p>{ count }</p>
+      <button onClick={ ()=> setCount( count + 1 ) }
+      >
+        incliment
+      </button>
+    </>
   )
 }
 
