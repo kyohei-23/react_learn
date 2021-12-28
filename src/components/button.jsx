a91d3f8ab1a37f67ed36d0f7b5cff843d95bd66c
kyoheiS <tgifz@icloud.com>
2021-12-28 12:24:06 +0900

    簡易的なダークモード変更機能実装

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function(){
     const [clicked, setClicked] = useState(false);
+    useEffect(() => {
+        return () => {
+            document.body.classList.toggle('dark')
+        };
+    }, [clicked]);
     return (
-        <button 
-            className={clicked ? 'hoge' : ''}
-            onClick={()=>setClicked( !clicked)  }
-        > 
-            click
-        </button>
+        <label>
+            { clicked ? '通常' : 'ダーク' }モードに変更
+            <button 
+                className={`toggle-btn ${clicked ? 'active' : ''}`}
+                onClick={()=>setClicked( !clicked)  }
+            > 
+                <span className="toggle-btn-body"></span>
+            </button>
+        </label>
     )
 }
